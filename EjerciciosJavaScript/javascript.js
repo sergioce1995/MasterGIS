6b8e4c9d36ef3ab423bdc98b3eecde4453f817b4
sergioce1995 <sergioce1995@gmail.com>
2023-02-07 19:50:37 +0100

    Create javascript.js

---
+++ EjerciciosJavaScript/javascript.js
@@ -0,0 +1,15 @@
+var ladoA;
+ladoA=16;
+var ladoB;
+ladoB=23;
+var ladoC;
+ladoC=13;
+var area=ladoA*ladoB*ladoC
+console.log(area)
+
+
+const celta = {
+    day: 07,
+    month: 'Agosto'
+};
+//Mi cumpleaños es el 07 de Agosto
