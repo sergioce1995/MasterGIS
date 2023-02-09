d24a3b591e641b341124bfd17713c88318fae5f8
sergioce1995 <sergioce1995@gmail.com>
2023-02-09 17:24:22 +0100

    Create Drone.js

---
+++ EjerciciosHTML/Drone.js
@@ -0,0 +1,8 @@
+var primeraOp = document.getElementById('first-option');
+
+
+var parrafo = document.getElementsByTagName('p';
+console.log('parrafo', parrafo));
+var labels = document.getElementsByTagName('label';
+console.log('parrafo', parrafo));
+
