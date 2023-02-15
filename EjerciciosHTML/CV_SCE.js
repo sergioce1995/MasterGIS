0c3f92e3b6988dddaa8e7753c4ca64e9663678d7
sergioce1995 <sergioce1995@gmail.com>
2023-02-15 15:59:03 +0100

    Create CV_SCE.js

---
+++ EjerciciosHTML/CV_SCE.js
@@ -0,0 +1,11 @@
+function toggleSection(experiencia) {
+  var section = document.getElementById(experiencia);
+  if (section.style.display === "none") {
+    section.style.display = "block";
+  } else {
+    section.style.display = "none";
+  }
+}
+  
+
+  
